4905b3d6b4d0efad3404b894a40f447abbfbf190
Quentin Gibson <quentingibson94@gmail.com>
2024-09-16 20:16:38 -0400

    add task command

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ import express from 'express';
 import {
   InteractionType,
   InteractionResponseType,
+  MessageComponentTypes,
   verifyKeyMiddleware,
 } from 'discord-interactions';
-import { getRandomEmoji } from './utils.js';
+import prisma from './prisma/index.js';
 
 // Create an express app
 const app = express();
@@ -32,22 +33,130 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
    * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
    */
   if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
+    // Slash command with name of "test"
 
-    // "test" command
-    if (name === 'test') {
-      // Send a message into the channel where command was triggered from
+    if (data.name === "remove_task") {
+      // Send a modal as response
+      console.log('sending modal')
       return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        type: InteractionResponseType.MODAL,
         data: {
-          // Fetches a random emoji to send from a helper function
-          content: `hello world ${getRandomEmoji()}`,
+          custom_id: "my_modal",
+          title: "Modal title",
+          components: [
+            {
+              // Text inputs must be inside of an action component
+              type: MessageComponentTypes.ACTION_ROW,
+              components: [
+                {
+                  // See https://discord.com/developers/docs/interactions/message-components#text-inputs-text-input-structure
+                  type: MessageComponentTypes.STRING_SELECT,
+                  custom_id: 'task_name',
+                  min_values: 1,
+                  options: [
+                    {
+                      label: "Task 1",
+                      value: "task_1",
+                      description: "Task 1 description",
+                      default: true
+                    },
+                    {
+                      label: "Task 2",
+                      value: "task_2",
+                      description: "Task 2 description",
+                    },
+                  ]
+                },
+              ],
+            },
+          ],
         },
       });
     }
 
-    console.error(`unknown command: ${name}`);
-    return res.status(400).json({ error: 'unknown command' });
+    if (data.name === 'add_task') {
+      // Send a modal as response
+      console.log('sending modal')
+      return res.send({
+        type: InteractionResponseType.MODAL,
+        data: {
+          custom_id: "my_modal",
+          title: "Modal title",
+          components: [
+            {
+              // Text inputs must be inside of an action component
+              type: MessageComponentTypes.ACTION_ROW,
+              components: [
+                {
+                  // See https://discord.com/developers/docs/interactions/message-components#text-inputs-text-input-structure
+                  type: MessageComponentTypes.INPUT_TEXT,
+                  custom_id: 'task_name',
+                  style: 1,
+                  label: "Type some text",
+                },
+              ],
+            },
+{
+              type: MessageComponentTypes.ACTION_ROW,
+              components: [
+                {
+                  type: MessageComponentTypes.INPUT_TEXT,
+                  custom_id: 'task_description',
+                  // Bigger text box for input
+                  style: 2,
+                  label: 'Type some (longer) text',
+                },
+              ],
+            },
+          ],
+        },
+      });
+    }
+  }
+
+  /**
+   * Handle modal submissions
+   */
+  if (type === InteractionType.MODAL_SUBMIT) {
+    // custom_id of modal
+    const modalId = data.custom_id;
+    // user ID of member who filled out modal
+    const userId = req.body.member.user.id;
+
+    if (modalId === "my_modal") {
+      let modalValues = "";
+      // Get value of text inputs
+      let taskName;
+      let taskDescription;
+      for (let action of data.components) {
+        let inputComponent = action.components[0];
+        modalValues += `${inputComponent.custom_id}: ${inputComponent.value}\n`;
+        if (inputComponent.custom_id === "task_name") {
+          taskName = inputComponent.value;
+        }
+        if (inputComponent.custom_id === "task_description") {
+          taskDescription = inputComponent.value;
+        }
+      }
+
+      console.log("checking for input");
+      if (taskName && taskDescription) {
+        console.log("Both exists");
+        await prisma.task.create({
+          data: {
+            name: taskName,
+            description: taskDescription,
+          },
+        });
+      }
+
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `<@${userId}> typed the following (in a modal):\n\n${modalValues}`,
+        },
+      });
+    }
   }
 
   console.error('unknown interaction type', type);
--- commands.js
+++ commands.js
@@ -26,6 +26,23 @@ const TEST_COMMAND = {
   contexts: [0, 1, 2],
 };
 
+
+const ADD_TASK_COMMAND = {
+  name: 'add_task',
+  description: 'Add a task for soldiers',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+const REMOVE_TASK_COMMAND = {
+  name: 'remove_task',
+  description: 'Remove a task for soldiers',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+}
+
 // Command containing options
 const CHALLENGE_COMMAND = {
   name: 'challenge',
@@ -44,6 +61,6 @@ const CHALLENGE_COMMAND = {
   contexts: [0, 2],
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+const ALL_COMMANDS = [ADD_TASK_COMMAND, REMOVE_TASK_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
