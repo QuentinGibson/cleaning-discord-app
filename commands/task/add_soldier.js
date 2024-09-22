4290295e49e648d1af341834f1604861a68f2085
Quentin Gibson <quentingibson94@gmail.com>
2024-09-22 12:58:12 -0400

    fix spelling errors in add soldier command

--- commands/task/add_soldier.js
+++ commands/task/add_soldier.js
@@ -5,7 +5,7 @@ const prisma = require('../../prisma');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('add_soldier')
-    .setDescription('Add a discord user to the list of availble soldiers.')
+    .setDescription('Add a discord user to the list of available soldiers.')
     .addUserOption((option) => 
       option
         .setName('soldier')
@@ -13,22 +13,22 @@ module.exports = {
         .setRequired(true)
     ),
 
-  async execute(interation) {
-    const userId = interation.options.getUser('soldier').id;
-    const username = interation.options.getUser('soldier').username;
+  async execute(interaction) {
+    const userId = interaction.options.getUser('soldier').id;
+    const username = interaction.options.getUser('soldier').username;
     try {
       await prisma.soldier.create({ data: { userId, username } });
-      await interation.reply({
+      await interaction.reply({
         content: 'Soldier added to the list!',
       });
     } catch (e) {
       if (e instanceof PrismaClientKnownRequestError && e.code === 'P2002') {
-        await interation.reply({
+        await interaction.reply({
           content: 'This user is already in the list of soldiers!',
         });
       } else {
         console.log(e);
-        await interation.reply({
+        await interaction.reply({
           content: 'Failed to add soldier to the list!',
         });
       }
